69ba641d5ae3d6846f28a2ed1a07a61c053f7ccb
杜雪迎 <702699388@qq.com>
2020-07-27 19:55:09 +0800

    //方法二：采用二分查找的方式，进行定位 //定义首尾下标，因为是非递减数组旋转，所以旋转最后可以看做成两部分，前半部分整体非递减，后半部分整体非递减，前 半部分整体大于后半部分。 //所以，我们假设如下定义，left指向最左侧，right指向最右侧，mid为二分之后的中间位置。 //则，a[mid] >= a[left],说明mid位置在原数组前半部分，进一步说明，目标最小值，在mid的右侧，让left=mid //a[mid] < a[left], 说明mid位置在原数组后半部分，进一步说明，目标最小值，在mid的左侧，让right=mid //这个过程，会让[left, right]区间缩小 //这个过程中，left永远在原数组前半部分，right永远在原数组的后半部分，而范围会一直缩小 //当left和right相邻时，right指向的位置，就是最小元素的位置 //但是，因为题目说的是非递减，也就意味着数据允许重复，因为有重复发，就可能会有a[left] == a[mid] == a[right]的情况，我们就无法判定数据在mid左侧还是右侧。（注意，只要有两者不相等，我们就能判定应该如何缩小范围）

---
+++ 每日一题/smallest.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+public class smallest {
+    public int minNumberInRotateArray(int [] array) {
+        if(array==null&&array.length==0){
+            return 0;
+        }
+        int left=0;
+        int right=array.length-1;
+        int mid=0;
+        while(array[left]>=array[right]){
+            if(right-left==1){
+                mid=right;
+                break;
+            }
+            mid=(left+right)/2;
+            if(array[left]==array[right]&&array[mid]==array[left]){
+                int result=array[left];
+                for(int i=left+1;i<right;i++){
+                    if(array[i]<result){
+                        result=array[i];
+                    }
+                }
+                return result;
+            }
+            if(array[mid]>=array[left]){
+                left=mid;
+            }
+            else{
+                right=mid;
+            }
+        }
+        return array[mid];
+    }
+}
